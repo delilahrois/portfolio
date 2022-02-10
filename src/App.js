7d86985a85ead93e8c9d93b066736ad33bb79f89
Delilah Necrason <delilahnecrason@gmail.com>
2022-02-10 11:29:03 -0500

    Import components to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home/Home';
+import Nav from './components/Nav/Nav';
+import Projects from './components/Projects/Projects';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 
 const App = () => {
 
