300821dede1f5d49d4d33de97b383b8b3336e031
Delilah Necrason <delilahnecrason@gmail.com>
2022-02-10 14:59:38 -0500

    Create welcome component and basic formatting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 import Home from './components/Home/Home';
@@ -6,15 +6,30 @@ import Nav from './components/Nav/Nav';
 import Projects from './components/Projects/Projects';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
+import Welcome from './components/Welcome/Welcome';
 
 const App = () => {
+
+  const [page, setPage] = useState(<Welcome />);
+
+  const loadingElement = () => {
+  
+    setTimeout(() => {
+      setPage(<Home/>)
+    }, 2000)
+
+  }
+
+  useEffect(() => {
+    loadingElement()
+  }, [])
 
 
   return (
     <div className="app">
       <Nav/>
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
+        <Route path="/" element={page}></Route>
         <Route path="/projects" element={<Projects/>}></Route>
         <Route path="/about" element={<About/>}></Route>
         <Route path="/contact" element={<Contact/>}></Route>
