0293de0993fd7015aa46300529129a50cd07106d
Delilah Necrason <delilahnecrason@gmail.com>
2022-02-11 10:59:44 -0500

    Fix contact info

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import './Contact.scss';
+import Nav from '../Nav/Nav';
 
 const Contact = () => {
 
 
   return (
-    <p>Contact page</p>
+    <>
+      <Nav/>
+      <div className="contact">
+      <h1 className="contact-header">Let's connect!</h1>
+      <div className="contact-info">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/2048px-LinkedIn_icon_circle.svg.png"></img><p>LinkedIn</p>
+        <p>GitHub</p>
+      </div>
+    </div>
+    </>
   )
 }
 
