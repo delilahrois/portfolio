32e87ff68b1d4af90ccece296385d20dd0fb45d2
Delilah Necrason <delilahnecrason@gmail.com>
2022-02-11 14:33:40 -0500

    Add about content and styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,12 +8,23 @@ const About = () => {
     <>
       <Nav/>
       <div className="about">
-      {/* <img src="https://media-exp1.licdn.com/dms/image/C4E03AQFOKegR7q7G2g/profile-displayphoto-shrink_800_800/0/1633904939929?e=1649894400&v=beta&t=lVXVGdPfljgVUaWLBjz6mkg767eyZ_Toj21Kl-P2HrE" alt="self-portrait" className="self-portrait"></img> */}
-      <div className="about-txt-container">
-        <h1>I am a person.</h1>
-        <p>Here is some info about me.</p>
+
+        <div className="about-txt-container">
+          <h1 className="skills">S k i l l s </h1>
+          <p className="skills-list">JavaScript | React.js | Router | Redux | Cypress | HTML5 | CSS3 | Sass | </p>
+          <p></p>
+
+
+          <p>I've always been a creator.</p>
+
+          <p>As a child I was active in the arts-- theatre, dance, music, drawing, language. In adulthood, my passion for creativity and giving nature led me to the hospitality industry, where I quickly found myself in a management position at a popular, fast-paced eatery in downtown Portland, Maine.</p>
+
+          <p>It was this experience that gave me a keen attention to detail, calm under pressure and knack for creative problem solving. I was content in the industry for a time, but something was missing in my life. I knew I wanted more.</p>
+
+          <h1>Where I'm going</h1>
+          <p>I am currently seeking a role that will allow me to flex my creative problem solving muscles and be part of a dynamic team of thinkers, doers and lifelong learners! I am eager to begin working on a project that will better the lives of humans and the world around us.</p>
+        </div>
       </div>
-    </div>
     </>
   )
 }
