af2a2a61317e436b2f8235c9b02ae4e8718b800f
Delilah Necrason <delilahnecrason@gmail.com>
2022-02-11 10:59:07 -0500

    Remove image from about page and add linkedin icon

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './About.scss';
+import Nav from '../Nav/Nav';
 
 const About = () => {
 
   return (
-    <div className="about">
-      <img src="https://media-exp1.licdn.com/dms/image/C4E03AQFOKegR7q7G2g/profile-displayphoto-shrink_800_800/0/1633904939929?e=1649894400&v=beta&t=lVXVGdPfljgVUaWLBjz6mkg767eyZ_Toj21Kl-P2HrE" alt="self-portrait" className="self-portrait"></img>
+    <>
+      <Nav/>
+      <div className="about">
+      {/* <img src="https://media-exp1.licdn.com/dms/image/C4E03AQFOKegR7q7G2g/profile-displayphoto-shrink_800_800/0/1633904939929?e=1649894400&v=beta&t=lVXVGdPfljgVUaWLBjz6mkg767eyZ_Toj21Kl-P2HrE" alt="self-portrait" className="self-portrait"></img> */}
       <div className="about-txt-container">
-        <h1>I never imagined I'd find myself in code.</h1>
+        <h1>I am a person.</h1>
+        <p>Here is some info about me.</p>
       </div>
     </div>
+    </>
   )
 }
 
